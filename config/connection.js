815ab08f3aedb99ec26a7cf1aad16d39261048a6
jon alden <jondevalden@gmail.com>
2019-09-27 10:36:56 -0500

    m

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
-var Sequelize = require("sequelize");
+// var Sequelize = require("sequelize");
 
-// Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("todo_db", "root", "root", {
-  host: "localhost",
-  port: 8889,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// // Creates mySQL connection using Sequelize
+// var sequelize = new Sequelize("todo_db", "root", "root", {
+//   host: "localhost",
+//   port: 8889,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
